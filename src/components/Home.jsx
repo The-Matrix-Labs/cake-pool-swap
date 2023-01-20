d8a6935da3bd4fd5c6d11a30ed33eb1c297f4d2f
HarshKhandelwal1552 <h2knitt@gmail.com>
2023-01-20 18:09:48 +0530

    unhide

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -355,7 +355,7 @@ function Home() {
             className="text-[#61ECFF]  cursor-pointer ss:text-[1.5rem] text-[1.2rem] ss:leading-[1.6rem] leading-[1.4rem] ss:w-[20%] w-full ss:text-center text-end"
           >
             <div className="flex flex-row items-center ss:justify-center justify-end gap-x-[15px]">
-              {show ? "Hide" : "Show"}
+              {show ? "Hide" : "Unhide"}
               <img
                 src={arrowDown}
                 alt=""
